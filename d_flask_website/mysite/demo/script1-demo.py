cb13c9b45a72eeccee575915fdb67b639a54f055
Jorge <alfred.jorge@gmail.com>
2023-02-27 18:15:40 -0300

    Minor corrections

--- d_flask_website/mysite/demo/script1-demo.py
+++ d_flask_website/mysite/demo/script1-demo.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
 
-app =Flask(__name__) #script executed __name__="__ main__" / imported __name__= "script1-demo" #name of file
+# script execute __name__="__ main__" / imported __name__= "script1-demo" the name of file
+app =Flask(__name__)
 
-@app.route('/') #route of link localhost:5000/(   )
+# route is localhost:5000/(   )
 
+
+@app.route('/')
 def home():
     return render_template("home.html")
 
-@app.route('/about/') 
 
+@app.route('/about/') 
 def about():
     return render_template("about.html")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
